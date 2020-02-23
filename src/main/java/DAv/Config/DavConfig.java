ba0b69f9bb5bdae7eda5f364f6422325cc493fe2
Nabeel Afsar <50879095+nadeveloper786@users.noreply.github.com>
2020-02-23 15:49:59 -0600

    changed C.S

--- src/main/java/DAv/Config/DavConfig.java
+++ src/main/java/DAv/Config/DavConfig.java
@@ -12,7 +12,7 @@ public class DavConfig {
 	@Bean
 	public DataSource  getDataSource() {
 		DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create()
-				.url("jdbc:sqlserver://meetup.database.windows.net:1433;database=Dav;user=nadeveloper@meetup;password={your_password_here};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;")
+				.url("jdbc:sqlserver://meetupdav.database.windows.net:1433;database=DavDB;user=nadeveloper786@meetupdav;password={your_password_here};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;")
 				.username("b1ed2f278da28f")
 				.password("cf0cf839");
 		return dataSourceBuilder.build();
